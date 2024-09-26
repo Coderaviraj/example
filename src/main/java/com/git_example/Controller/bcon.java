245b6f419aadbc15d87c63c915435d0ecc6419fa
Ravi <coderaviraj@gmail.com>
2024-09-26 10:54:59 +0530

    manager changer after debug

--- src/main/java/com/git_example/Controller/bcon.java
+++ src/main/java/com/git_example/Controller/bcon.java
@@ -9,5 +9,13 @@ public class bcon {
         System.out.println("This is a change from master branch.");
         System.out.println("This is a change from develop branch.");
         System.out.println("100");
+
+        bcon a1 = new bcon();
+        int val = a1.test2();
+        System.out.println(val);
+    }
+
+    private int test2() {
+        return 10;
     }
 }
